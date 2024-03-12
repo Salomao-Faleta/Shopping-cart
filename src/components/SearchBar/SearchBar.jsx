808fd28c841d44aa09be5faddbd9772bf0eb3796
Salomao-Faleta <faletas41@gmail.com>
2024-03-12 11:34:07 -0300

    Fazendo a barra de pesquisa funcionar- Part.4

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,26 @@
-import { React, useState} from 'react';
-
+import { React, useState } from 'react';
 import './SearchBar.css';
-
 //Icone de lupa do header
 import { IoSearch } from 'react-icons/io5';
+import { fetchProducts } from '../../api/fetchProducts';
 
 export function SearchBar() {
 
     const [searchValue, setSearchValue] = useState('');
 
+    //Barra de pesquisa
+    const handleSearch = async (e) => {
+        e.preventDefault();
+
+        const products = await fetchProducts(searchValue);
+
+        setSearchValue('');
+
+        console.log(products);        
+    };
+
     return (
-        <form className="search-bar">
+        <form className="search-bar" onSubmit={handleSearch}>
             <input
                 type="search"
                 className="search__input"
